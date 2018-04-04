71fe6ddc42ffb08f185356fdb297b44eb5126b03
katiro <katirox9@gmail.com>
2018-04-04 14:13:18 -0600

    regresar

--- ResBar/src/main/java/sv/edu/uesocc/ingenieria/tpi_2018/paractica/resbar/OrdenesController.java
+++ ResBar/src/main/java/sv/edu/uesocc/ingenieria/tpi_2018/paractica/resbar/OrdenesController.java
@@ -5,9 +5,17 @@
  */
 package sv.edu.uesocc.ingenieria.tpi_2018.paractica.resbar;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -16,9 +24,16 @@ import javafx.fxml.Initializable;
  */
 public class OrdenesController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+   @FXML
+    private void regresarIndex(ActionEvent e) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/index.fxml"));
+        Scene index = new Scene(root);
+        Stage stage2= (Stage)((Node) e.getSource()).getScene().getWindow();
+        stage2.setTitle("ResBar");
+        stage2.setScene(index);
+        stage2.show();
+
+    }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
